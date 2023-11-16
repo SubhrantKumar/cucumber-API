ca108ad4aecceec70f49041d69a08b5207a12d43
Subhrant <sahusubhranta@gmail.com>
2023-11-16 22:43:09 +0530

    Updated framework

--- APIFramework/src/test/java/cucumber/Options/TestRunner.java
+++ APIFramework/src/test/java/cucumber/Options/TestRunner.java
@@ -1,12 +1,11 @@
 package cucumber.Options;
 
 import org.junit.runner.RunWith;
-
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/java/features", glue= {"stepDefinations"},tags="@AddPlaceScenario" )
+@CucumberOptions(features="src/test/java/features", plugin="json:target/jsonReports/cucumber-report.josn",glue= {"stepDefinations"} )
 //tags= "@DeletePlaceScenario"
 public class TestRunner {
 
